33ac9b7b48ea479078c2e6034491f52f82e2b0d3
Chris Scutcher <chris@scutcher.uk>
2018-04-26 16:11:48 +0100

    Use find instead of globbing
    Using globbing was blowing up when starting xfce.

--- sh/modules/profile_d.sh
+++ sh/modules/profile_d.sh
@@ -1,7 +1,7 @@
 # Load any profiles in ~/.profile.d
 if [ -d $HOME/.profile.d ]; then
     log "Looking for profile.d"
-    for f in *.profile; do
+    for f in `find -L $HOME/.profile.d -name '*.profile' -type f -readable`; do
         log "Loading profile: $f"
         source $f
     done
